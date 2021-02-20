9289ccc73f61d9d7ea6594c3836252f9086960e0
Dennis Sarmiento <dennissarmiento@Denniss-MBP.fios-router.home>
2021-02-20 16:32:11 -0500

    up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 // import NavBar from './components/Navbar/NavBar';
 // import Sidebar from './components/Sidebar/Sidebar';
-import { HashRouter, Route, Switch } from "react-router-dom"
+// import { HashRouter, Route, Switch } from "react-router-dom"
 
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import SigninPage from './pages/SigninPage';
 import Home from './pages/Home';
 
@@ -11,12 +11,12 @@ import Home from './pages/Home';
 
 function App() {
   return (
-    <HashRouter>
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/signin" component={SigninPage} exact />
       </Switch>
-    </HashRouter>
+    </Router>
   );
 }
 
